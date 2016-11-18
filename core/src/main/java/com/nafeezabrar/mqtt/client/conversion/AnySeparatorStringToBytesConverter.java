58e569fcf951869912a58f2ad9ac5550e9bf3a4f
Nafeez Abrar <nafeez.abrar@aplombtechbd.com>
2016-11-18 18:23:26 +0600

    String to bytes converter is restricted to only for hexadecimal and decimal

--- core/src/main/java/com/nafeezabrar/mqtt/client/conversion/AnySeparatorStringToBytesConverter.java
+++ core/src/main/java/com/nafeezabrar/mqtt/client/conversion/AnySeparatorStringToBytesConverter.java
@@ -4,6 +4,8 @@ import java.util.StringTokenizer;
 
 public class AnySeparatorStringToBytesConverter implements StringToBytesConverter {
 
+    private final String hexRegex = "^0[xX][\\da-fA-F]+$";
+
     @Override
     public byte[] convertStringToBytes(String message) {
         StringTokenizer tokenizer = new StringTokenizer(message, ", \t\n:-");
@@ -11,7 +13,11 @@ public class AnySeparatorStringToBytesConverter implements StringToBytesConverte
         byte[] bytes = new byte[tokenizer.countTokens()];
         for (int i = 0; tokenizer.hasMoreTokens(); i++) {
             String token = tokenizer.nextToken();
-            bytes[i] = Integer.decode(token).byteValue();
+            if (token.matches(hexRegex)) {
+                bytes[i] = Integer.decode(token).byteValue();
+            } else {
+                bytes[i] = (byte) Integer.parseInt(token);
+            }
         }
 
         return bytes;
